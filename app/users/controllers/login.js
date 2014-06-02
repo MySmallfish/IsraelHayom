480d7ddac1686abf1e20007fd8aa9de8d7d9c30f
Yair Cohen <mysmallfish@gmail.com>
2014-06-02 17:12:16 +0300

    Login - only show alert with the name

--- app/users/controllers/login.js
+++ app/users/controllers/login.js
@@ -1,8 +1,15 @@
 ﻿(function (S, I) {
     I.LoginController = ["$scope", "$q", "$location", function ($scope, $q, $location) {
 
-        $scope.openProfile = function() {
+        $scope.openProfile = function () {
             $location.path("/Profile");
+        };
+
+        $scope.alertLogin = function (item) {
+            alert("התחברת עם " + item);
+
+            $location.path("/");
+
         };
 
     }];
